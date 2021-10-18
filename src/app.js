6d10bb0d61e1a69ae17719b5bbdd8e65131327a7
henimex <49072861+henimex@users.noreply.github.com>
2021-10-18 23:42:38 +0300

    Krediler

---
+++ src/app.js
@@ -0,0 +1,3 @@
+console.log("Hello Kodlama.io")
+
+let konutKredileri = ["Bir Kendine Bir Bankaya İki Devlete Araç Al Kredisi", "Paraya İhtiyacım Var Borç Ver Dahada ihtiyacım Olsun Kredisi","Dolara Karşı Mücadele Kredisi","Çocuğunu Okutmak İstiyorsan Donuna Kadar Alırız Kredisi."]
