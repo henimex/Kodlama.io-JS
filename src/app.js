7d7dc8322a219ba33f23cad3673776881f952356
henimex <49072861+henimex@users.noreply.github.com>
2021-10-19 00:00:49 +0300

    JS Day 01 End

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
-console.log("Hello Kodlama.io")
+console.log("Hello JavaScript with Kodlama.io by @engindemirog");
 
-let konutKredileri = ["Bir Kendine Bir Bankaya İki Devlete Araç Al Kredisi", "Paraya İhtiyacım Var Borç Ver Dahada ihtiyacım Olsun Kredisi","Dolara Karşı Mücadele Kredisi","Çocuğunu Okutmak İstiyorsan Donuna Kadar Alırız Kredisi."]
+let krediler = [
+  "Bir Kendine Bir Bankaya İki Devlete Araç Al Kredisi",
+  "Paraya İhtiyacım Var Borç Ver Dahada ihtiyacım Olsun Kredisi",
+  "Dolara Karşı Anlamsız Mücadele Kredisi",
+  "Çocuğunu Okutmak İstiyorsan Donuna Kadar Alırız Kredisi.",
+  "Bir Sıfırda Sen At Dönsün Dolaşsın Sana Girsin Kredisi",
+  "Yolda Yürüyorsan Vergisini Verecek Veremezsen Kredi Veririz Kredisi",
+  "Tarih Belirt Çalışırken Çırpmaya Devam Et Kredisi",
+  "Fakirin Yüzü Güler mi? Güldürürüz Kredisi"
+];
+
+for (let i = 0; i < krediler.length; i++) {
+  const element = krediler[i];
+  console.log(element);
+}
