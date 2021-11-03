e98ff6506bc7c5d9178ad81c6c53685a9b8cf38b
henimex <49072861+henimex@users.noreply.github.com>
2021-11-03 20:55:07 +0300

    Homeworrk

---
+++ src/HomeWorks/Day02/homework.js
@@ -0,0 +1,10 @@
+let sectionTite = "Homeworks"
+let dayInfo = "Day 2"
+
+function chekcPrimeNumbers(...numbers) {
+    numbers.forEach(number => {
+        console.log(number)
+    });
+}
+
+chekcPrimeNumbers(10,15,26,66)
