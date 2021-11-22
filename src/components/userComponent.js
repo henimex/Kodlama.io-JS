6c692b2431b691f6ac8bc296f12afd30cbd1a121
henimex <henimex@gmail.com>
2021-11-23 00:52:57 +0300

    Spagetti For Refactor

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,6 +1,6 @@
 import UserService from "../services/userService.js";
 import User from "../models/user.js";
-import {FileLogger} from "../crossCuttingConcerns/logging/fileLogger.js";
+import Customer from "../models/customer.js";
 import {ElasticLogger} from "../crossCuttingConcerns/logging/elasticLogger.js";
 
 let userService = new UserService(new ElasticLogger());
@@ -9,11 +9,22 @@ let user1 = new User(1,"Joseph", "Henimex", "New York");
 let user2 = new User(2,"Arnold", "Josh", "London");
 let user3 = new User(3,"Danny", "Cittaer", "Moscow");
 let user4 = new User(4,"Maria", "Pillow", "Las Vegas");
+let user5 = new User(4,"Keity", "Feller", "Tokyo");
 
-userService.add(user1);
-userService.add(user2);
-userService.add(user3);
-userService.add(user4);
-userService.getById(2);
-console.log(userService.list());
+//userService.add(user1);
+//userService.add(user2);
+//userService.add(user3);
+//userService.add(user4);
+//userService.getById(2);
+//console.log(userService.list());
+let customerToAdd = new Customer(1,"Poul","Taylor","Ibiza", 26,"qwedq")
+customerToAdd.type = "customer"
+console.log(userService.add(customerToAdd))
+userService.loadData();
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(Number.parseInt("21basdsa..sdçsa.dçsa9"))
+
+
+
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,28 +1,128 @@
-
+import { users } from "../data/users.js";
+import DataError from "../models/dataError.js";
 
 export default class UserService {
-    constructor(logger) {
-        this.logger = logger
-        this.users = []
+  constructor(logger) {
+    this.logger = logger;
+    this.employees = [];
+    this.customers = [];
+    this.errors = [];
+  }
+
+  loadData() {
+    for (const user of users) {
+      switch (user.type) {
+        case "customer":
+          if (!this.checkCustomerValidity(user)) {
+            this.customers.push(user);
+          }
+          break;
+        case "employee":
+          if (!this.checkEmployeeValidity(user)) {
+            this.employees.push(user);
+          }
+          break;
+        default:
+          this.errors.push(new DataError("Wrong Data User", user));
+          break;
+      }
     }
 
-    add(user) {
-        this.users.push(user)
-        this.logger.log("User Add Operation Called By Poul Tylon")
-        console.log("User Added ", user)
+    console.log(this.errors);
+  }
+
+  /*
+  *Refacotr için Error Sınıfı Olusturulacak, 
+  Validate Sınıfı Olusturulacak, 
+  Bunların Modelleri olusturulacak. 
+  Servis icersindeki ekleme islemi merkezi bir validasyonda gerçekleşip eger validasyondan geçmez ise validasyon içerisinden error sınıfına yönlendirme yapılacak.
+  iç içe fonksiyonların hepsi function a çevrilecek !
+
+  Switch Case yapısı için Interface kullanılmaya calısılacak. EC5+ Support Bakılacak? 
+  */
+
+  checkCustomerValidity(user) {
+    let requiredFields = "id firstName lastName age city".split(" ");
+    let hasErrors = false;
+    for (const field of requiredFields) {
+      if (!user[field]) {
+        this.errors.push(
+          new DataError(`Validation Problem! ${field} is required.`, user)
+        );
+        hasErrors = true;
+      }
     }
 
-    list() {
-        console.log("Users Listed")
-        this.logger.log("User List Operation Called By Poul Tylon")
-        return this.users;
-
+    if (Number.isNaN(Number.parseInt(user.age))) {
+      this.errors.push(
+        new DataError(`Validation Problem! ${user.age} is not a number.`, user)
+      );
     }
 
-    getById(id) {
-        let requestedUser = this.users.find(x => x.id === id)
-        this.logger.log(`User Get(${id}) Operation Called By Poul Tylon`)
-        console.log(" User Found")
-        return requestedUser
+    return hasErrors;
+  }
+
+  checkEmployeeValidity(user) {
+    let requiredFields = "id firstName lastName age city salary".split(" ");
+    let hasErrors = false;
+    for (const field of requiredFields) {
+      if (!user[field]) {
+        this.errors.push(
+          new DataError(`Validation Problem! ${field} is required.`, user)
+        );
+        hasErrors = true;
+      }
     }
+
+    if (Number.isNaN(Number.parseInt(user.age))) {
+      this.errors.push(
+        new DataError(`Validation Problem! ${user.age} is not a number.`, user)
+      );
+    }
+
+    return hasErrors;
+  }
+
+  loadDataRefactored() {
+    this.employees = null;
+  }
+
+  add(user) {
+    //this.users.push(user);
+    //this.logger.log("User Add Operation Called By Poul Tylon");
+    //console.log("User Added ", user);
+
+    //Spagetti
+
+    switch (user.firstName) {
+      case "customer":
+        if (!this.checkCustomerValidity(user)) {
+          this.customers.push(user);
+          break;
+        }
+
+      case "employee":
+        if (!this.checkEmployeeValidity(user)) {
+          this.employees.push(user);
+          break;
+        }
+
+      default:
+        this.errors.push(new DataError(`Wrong User Type`));
+        break;
+    }
+  }
+
+  list() {
+    console.log("Users Listed");
+    this.logger.log("User List Operation Called By Poul Tylon");
+    return this.users;
+  }
+
+  getById(id) {
+    let requestedUser = this.users.find((x) => x.id === id);
+    this.logger.log(`User Get(${id}) Operation Called By Poul Tylon`);
+    console.log(" User Found");
+    return requestedUser;
+  }
 }
